5732980622b79a4cc89c6cead9250fa29d0e911a
Ashish D'Souza <sudouser512@gmail.com>
2020-05-10 18:25:14 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,8 +54,8 @@ var files = {
     },
     "projects": {
       "timelock.txt": "\n%+a%+kTimeLock%-k%-a | May '20\n" +
-          "Description: "A database-oriented implementation of time-lock encryption, which relies on a secure, verified third party to prevent falsified decryption attempts. This was developed using Java, Spring Boot, and PostgreSQL. Time-lock encryption is still a theoretical concept, and has yet to become a true, stable implementation.\n" +
-          "Creators: Ashish D'SOuza\n\n",
+          "Description: A database-oriented implementation of time-lock encryption, which relies on a secure, verified third party to prevent falsified decryption attempts. This was developed using Java, Spring Boot, and PostgreSQL. Time-lock encryption is still a theoretical concept, and has yet to become a true, stable implementation.\n" +
+          "Creators: Ashish D'Souza\n\n",
       "covid-19_survival_calculator.txt": "\n%+a%+kCOVID-19 Survival Calculator%-k%-a | Mar '20\n" +
           "Description: This calculator is a web application that allows users who have coronavirus (COVID-19) to calculate " +
           "the probability of their survival. We devised a machine learning algorithm that can\naccurately predict this value, " +
